dd42f25a04432347b797a8a27fabccec8ad1421e
Nektaria <n.verikokkou@gmail.com>
2024-02-29 18:14:50 +0200

    1η εργαστηριακή

--- lab1.py
+++ lab1.py
@@ -1,17 +1,34 @@
-import requests  # εισαγωγή της βιβλιοθήκης
+import requests
+import http.cookies
 
-def more(text):
-    count = 0
-    for line in text.split('\n'):
-        print(line)
-        count += 1
-        if count % 30 == 0:
-            reply = input('Show more (y/n)? ')
-            if reply == 'n':
-                break
+while True:
+    url = input('URL:')
+    if url == '': break
+    if not url.startswith('http'):
+        url = 'http://'+ url
+    try:
+        with requests.get(url) as response:
+            print("\nRESPONSE STATUS: ", response.status_code)
+        print("RESPONSE HEADER")
+        for key, value in response.headers.items():
+            print("{:30s} {}".format(key, value))
+        if 'Server' in response.headers:
+            print("\n\nWeb server: " + response.headers['Server'])
+        else:
+            print("\nWeb server not found.")
+    except:
+        print('error opening', url)
 
-url = 'http://python.org/'  # προσδιορισμός του url
+    if 'Set-Cookie' in response.headers:
+        print("\nThis page uses cookies.")
 
-with requests.get(url) as response: 
-    html = response.text
-    more(html)
+        raw_cookies = response.headers['Set-Cookie']
+        cookies = http.cookies.SimpleCookie(raw_cookies)
+
+        print("\nCookies information:")
+        for key, morsel in cookies.items():
+            print("Name: " + key)
+            print("Valid until: " + morsel['expires'])
+            print()
+    else:
+        print("\nThis page does not use cookies.")
