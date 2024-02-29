355c18166f4ed213cc087a6aa1cca8a3a74fbf09
Nektaria <n.verikokkou@gmail.com>
2024-02-29 21:21:49 +0200

    1η εργαστηριακή

--- lab1.py
+++ lab1.py
@@ -8,27 +8,24 @@ while True:
         url = 'http://'+ url
     try:
         with requests.get(url) as response:
-            print("\nRESPONSE STATUS: ", response.status_code)
-        print("RESPONSE HEADER")
-        for key, value in response.headers.items():
-            print("{:30s} {}".format(key, value))
-        if 'Server' in response.headers:
-            print("\n\nWeb server: " + response.headers['Server'])
-        else:
-            print("\nWeb server not found.")
+            print("RESPONSE HEADER")
+            for key, value in response.headers.items():
+                print("{:30s} {}".format(key, value))
+
+            server = response.headers.get('Server')
+            if server:
+                print(f"\n\nWeb server: {server}")
+            else:
+                print("\nWeb server not found.")
+
+            if 'Set-Cookie' in response.headers:
+                print("\nCookies information:")
+                raw_cookies = response.headers['Set-Cookie']
+                cookies = http.cookies.SimpleCookie(raw_cookies)
+                for key, value in cookies.items():
+                    print("Name: " + key)
+                    print("Valid until: " + value['expires'])
+            else:
+                print("No cookies found")
     except:
         print('error opening', url)
-
-    if 'Set-Cookie' in response.headers:
-        print("\nThis page uses cookies.")
-
-        raw_cookies = response.headers['Set-Cookie']
-        cookies = http.cookies.SimpleCookie(raw_cookies)
-
-        print("\nCookies information:")
-        for key, morsel in cookies.items():
-            print("Name: " + key)
-            print("Valid until: " + morsel['expires'])
-            print()
-    else:
-        print("\nThis page does not use cookies.")
